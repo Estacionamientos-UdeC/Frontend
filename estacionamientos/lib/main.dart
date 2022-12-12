412f4b7a0d4962fb03c7fb424ccf942a506bd932
Diego Iturra <diturra@dparadig.com>
2022-12-12 11:11:41 -0300

    Se actualizan dependencias del proyecto y se elimina el uso de providers para realizar peticiones Http

--- estacionamientos/lib/main.dart
+++ estacionamientos/lib/main.dart
@@ -1,38 +1,22 @@
-import 'package:estacionamientos/providers/parking_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'screens/home_screen.dart';
 
-void main() {
-  runApp(const AppState());
-}
 
-class AppState extends StatelessWidget{
-  const AppState({Key? key}) : super(key: key);
-  
-  @override
-  Widget build(BuildContext context) {
-    return MyApp();
-  }
-
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Estacionamientos App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.indigo
       ),
       initialRoute: '/home',
       routes: {
-        '/home' : (context) => const HomeScreen()
+        '/home' : (context) => HomeScreen()
       },
     );
   }
